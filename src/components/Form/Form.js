2e96d300c86409a3e4387dfd79383899cb98c4df
Dmitry <d.shishkalov@directline.digital>
2022-08-02 15:52:47 +0400

    Add form validation

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,51 +1,90 @@
-import React from "react";
-
+import { React, useState } from "react";
+import styles from "./Form.module.scss";
 import FormItem from "./FormItem";
 
 const Form = () => {
-	const formData = [
-		{
-			id: 1,
-			title: 'Ваш город',
-			isSelect: true
-		},
-		{
-			id: 2,
-			title: 'Пароль',
-			isSelect: false,
-			type: 'password',
-			description: 'Ваш новый пароль должен содержать не менее 5 символов.'
-		},
-		{
-			id: 3,
-			title: 'Пароль еще раз',
-			isSelect: false,
-			type: 'password',
-			description: 'Повторите пароль, пожалуйста, это обезопасит вас с нами на случай ошибки.'
-		},
-		{
-			id: 4,
-			title: 'Электронная почта',
-			isSelect: false,
-			type: 'email',
-			description: 'Можно изменить адрес, указанный при регистрации.'
-		},
-		{
-			id: 5,
-			title: 'Я согласен',
-			isSelect: false,
-			type: 'checkbox',
-			description: 'принимать актуальную информацию на емейл'
-		},
-	]
+  const formData = [
+    {
+      id: 1,
+      title: "Ваш город",
+      isSelect: true,
+      name: "city",
+    },
+    {
+      id: 2,
+      title: "Пароль",
+      isSelect: false,
+      type: "password",
+      description: "Ваш новый пароль должен содержать не менее 5 символов.",
+      name: "password",
+			errorText: 'Используйте не менее 5 символов'
+    },
+    {
+      id: 3,
+      title: "Пароль еще раз",
+      isSelect: false,
+      type: "password",
+      description:
+        "Повторите пароль, пожалуйста, это обезопасит вас с нами на случай ошибки.",
+      name: "repeatPassword",
+			errorText: 'Пароли не совпадают'
+    },
+    {
+      id: 4,
+      title: "Электронная почта",
+      isSelect: false,
+      type: "email",
+      description: "Можно изменить адрес, указанный при регистрации.",
+      name: "email",
+			errorText: 'Неверный E-mail'
+    },
+    {
+      id: 5,
+      title: "Я согласен",
+      isSelect: false,
+      type: "checkbox",
+      description: "принимать актуальную информацию на емейл",
+			name: "agreement"
+    },
+  ];
+
+	const [formValues, setFormValues] = useState({city: 'Красноярск', password:'', repeatPassword:'', email:'', agreement: 'off'});
+	const [formErrors, setFormErrors] = useState({});
+
+	const getErrors = formValues => {
+		const errorObj = {};
+		if (formValues.password.length < 5) {
+			errorObj.password = true;
+		}
+		if (formValues.repeatPassword !== formValues.password) {
+			errorObj.repeatPassword = true;
+		}
+		if (formValues.email.length < 5 || !formValues.email.includes('@')) {
+			errorObj.email = true;
+		}
+
+		setFormErrors(errorObj);
+		Object.keys(errorObj).length === 0 && console.log(formValues);
+	}
+
+	const formSubmit = e => {
+		e.preventDefault();
+		getErrors(formValues)
+	}
 
   return (
-		<form>
-			{formData.map((item) => (
-				<FormItem data={item} key={item.id} />
-			))}
-		</form>
-	)
-}
+    <form onSubmit={formSubmit}>
+      {formData.map((item) => (
+        <FormItem data={item} key={item.id} formValues={formValues} setFormValues={setFormValues} formErrors={formErrors} />
+      ))}
+      <div className={styles.FormSubmit}>
+        <button className={styles.Button}>Изменить</button>
+        <p className={styles.Description}>
+          последние изменения 15 мая 2012 в 14:55:17
+        </p>
+      </div>
+    </form>
+  );
+};
 
 export default Form;
--- src/components/Form/FormItem.js
+++ src/components/Form/FormItem.js
@@ -1,26 +1,174 @@
 import React from "react";
+import classNames from "classnames";
 
 import styles from "./FormItem.module.scss";
 
 const FormItem = (props) => {
   const {
-    data: { title, isSelect, type, description },
+    data: { title, isSelect, type, description, name, errorText },
+    formValues,
+    setFormValues,
+    formErrors,
   } = props;
 
+  const cities = [
+    {
+      city: "Норильск",
+      population: "179554",
+    },
+    {
+      city: "Артёмовск",
+      population: "1688",
+    },
+    {
+      city: "Ачинск",
+      population: "105259",
+    },
+    {
+      city: "Боготол",
+      population: "19819",
+    },
+    {
+      city: "Бородино",
+      population: "16061",
+    },
+    {
+      city: "Дивногорск",
+      population: "29195",
+    },
+    {
+      city: "Дудинка",
+      population: "21015",
+    },
+    {
+      city: "Енисейск",
+      population: "17805",
+    },
+    {
+      city: "Железногорск",
+      population: "83857",
+    },
+    {
+      city: "Заозёрный",
+      population: "10286",
+    },
+    {
+      city: "Зеленогорск",
+      population: "61915",
+    },
+    {
+      city: "Игарка",
+      population: "4417",
+    },
+    {
+      city: "Иланский",
+      population: "14967",
+    },
+    {
+      city: "Канск",
+      population: "89111",
+    },
+    {
+      city: "Кодинск",
+      population: "15911",
+    },
+    {
+      city: "Красноярск",
+      population: "1095286",
+    },
+    {
+      city: "Лесосибирск",
+      population: "59525",
+    },
+    {
+      city: "Минусинск",
+      population: "68007",
+    },
+    {
+      city: "Назарово",
+      population: "49825",
+    },
+    {
+      city: "Сосновоборск",
+      population: "40614",
+    },
+    {
+      city: "Ужур",
+      population: "15563",
+    },
+    {
+      city: "Уяр",
+      population: "11981",
+    },
+    {
+      city: "Шарыпово",
+      population: "37136",
+    },
+  ];
+
+  const sortCities = [];
+  cities.forEach((city) => {
+    city.population > 50000 && sortCities.push(city);
+  });
+
+  function sortCitiesNames(x, y) {
+    if (x.city < y.city) {
+      return -1;
+    }
+    if (x.city > y.city) {
+      return 1;
+    }
+    return 0;
+  }
+
+  sortCities.sort(sortCitiesNames);
+
+  const maxObj = sortCities.reduce((acc, curr) =>
+    +acc.population > +curr.population ? acc : curr
+  );
+
+  function sortCitiesPopulation(x, y) {
+    return x === maxObj ? -1 : y === maxObj ? 1 : 0;
+  }
+
+  sortCities.sort(sortCitiesPopulation);
+
   return (
-    <div className={styles.Item}>
+    <div
+      className={classNames(
+        styles.Item,
+        type === "checkbox" && styles.Item_Checkbox
+      )}
+    >
       <div className={styles.Wrapper}>
-        <label className={styles.Label}>{title}</label>
+        <label className={styles.Label} htmlFor={title}>
+          {title}
+        </label>
         {isSelect ? (
-          <select className={styles.Input}>
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
+          <select
+            className={styles.Input}
+            name={name}
+            onChange={(event) =>
+              setFormValues({ ...formValues, [name]: event.target.value })
+            }
+          >
+            {sortCities.map((city) => (
+              <option key={city.population}>{city.city}</option>
+            ))}
           </select>
         ) : (
-          <input type={type} className={styles.Input} />
+          <input
+            id={title}
+            type={type}
+            className={classNames(styles.Input, formErrors[name] && styles.Input_Error)}
+            name={name}
+            onChange={(event) =>
+              setFormValues({ ...formValues, [name]: event.target.value })
+            }
+          />
         )}
       </div>
+      {formErrors[name] && <p className={styles.Error}>{errorText}</p>}
       <p className={styles.Description}>{description}</p>
     </div>
   );
